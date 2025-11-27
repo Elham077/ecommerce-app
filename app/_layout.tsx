001a13f6b5f585c60c4d444e974d4f5a231dc193
elham077 <kohistani.taj.786@gmail.com>
2025-11-27 16:37:33 +0430

    commit 1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,35 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useFonts } from "expo-font"; // <-- اضافه شد
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import "react-native-reanimated";
+import "./global.css";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+  const [fontsLoaded] = useFonts({
+    Rubik_Bold: require("../assets/fonts/Rubik-Bold.ttf"),
+    Rubik_Regular: require("../assets/fonts/Rubik-Regular.ttf"),
+    Rubik_Medium: require("../assets/fonts/Rubik-Medium.ttf"),
+    Rubik_Light: require("../assets/fonts/Rubik-Light.ttf"),
+    Rubik_ExtraBold: require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    Rubik_SemiBold: require("../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+  useEffect(() => {
+    if (fontsLoaded) {
+      // Fonts are loaded, you can perform any additional actions here if needed
+      SplashScreen.hideAsync(); //
+    }
+  }, [fontsLoaded]);
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="modal"
+        options={{ presentation: "modal", title: "Modal" }}
+      />
+    </Stack>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+    module.exports = {
+      content: ["./components.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+      theme: {
+        extend: {
+          fontFamily: {
+            rubik: ["Rubik_Regular", "sans-serif"],
+            rubikBold: ["Rubik_Bold", "sans-serif"],
+            rubikSemiBold: ["Rubik_SemiBold", "sans-serif"],
+            rubikMedium: ["Rubik_Medium", "sans-serif"],
+            rubikLight: ["Rubik_Light", "sans-serif"],
+            rubikExtraBold: ["Rubik_ExtraBold", "sans-serif"],
+          },
+        },
+      },
+      plugins: [],
+    };
